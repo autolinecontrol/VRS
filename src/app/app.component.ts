8b4d9a9a11c16ca983f61aecb90d93187366ea8c
David <dlcabezas2@gmail.com>
2020-02-14 12:09:09 -0500

    Login with Firebase
    El proyecto tiene el login con firebase

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth,  User } from 'firebase/app';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'VRS';
+  usuario: User;
+  cargando: boolean =  true;
+  constructor(private auth: AngularFireAuth) {
+  this.auth.user.subscribe((usuario) =>   {
+
+    setTimeout(() => {
+      this.cargando  = false;
+      this.usuario = usuario;
+    }, 1000);
+
+  });
+  }
+  login() {
+    this.auth.signInWithEmailAndPassword('dlcabezas2@gmail.com', 'firebase2020' );
+  }
+  logout() {
+    this.auth.signOut();
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule} from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AccordionModule.forRoot(),
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    ReactiveFormsModule
+  ],
+  providers: [
+    AngularFireAuth
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
