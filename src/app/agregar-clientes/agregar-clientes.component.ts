527e2c7589ec9d6d962c7cdf591e6d119f27ae11
David <dlcabezas2@gmail.com>
2020-02-17 17:49:37 -0500

    Crea los usarios con llave y muestra fotos
    Se logra crear los usuarios con la llave del correo y ya muestra las fotos en un array

--- src/app/agregar-clientes/agregar-clientes.component.ts
+++ src/app/agregar-clientes/agregar-clientes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/storage';
 import 'firebase/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 
 
@@ -12,8 +13,11 @@ import 'firebase/storage';
 })
 export class AgregarClientesComponent implements OnInit {
   formularioClientes: FormGroup
-  constructor(private fb: FormBuilder,private storage: AngularFireStorage) { }
+  constructor(private fb: FormBuilder,private storage: AngularFireStorage,private db: AngularFirestore) { }
   porcentajeSubida:number =0
+  arreglo: any = new Array<any>();
+  correo: string
+  a = new Object();
   ngOnInit() {
     this.formularioClientes=this.fb.group({
       Nombre: ['',Validators.required],
@@ -24,27 +28,62 @@ export class AgregarClientesComponent implements OnInit {
       Facultad: ['',Validators.required],
       Iden: ['',Validators.required],
       Role: ['',Validators.required],
-      img: ['',Validators.required],
       Carrera: ['']
+      
+      
     })
   }
   agregar(){
     console.log(this.formularioClientes.value)
+    let correo: string=this.formularioClientes.value.Email
+    let nombre: string=this.formularioClientes.value.Nombre
+    let apellido: string=this.formularioClientes.value.Apellido
+    let facultad: string=this.formularioClientes.value.Facultad
+    let iden: string=this.formularioClientes.value.Iden
+    let role: string=this.formularioClientes.value.Role
+    let carrera: string=this.formularioClientes.value.Carrera
+    this.db.collection("Usuarios").doc(correo).set({
+      Nombre: this.formularioClientes.value.Nombre,
+      Apellido: apellido,
+      Email: correo,
+      Facultad: facultad,
+      Iden: iden,
+      Role: role,
+      Carrera: carrera
+  })
+  .then(function() {
+      console.log("Document successfully written!");
+  })
+  .catch(function(error) {
+      console.error("Error writing document: ", error);
+  });
+    // // let upload=JSON.parse(subir);
+    // // console.log(upload)  
+    //      this.db.collection('Usuarios').add(subir).then((termino)=>{
+    //      console.log('Usuario Registrado')
+    //  })
+    
   }
   subirImagen(evento)
   {
-    let archivo = evento.target.files[0]
-    let ruta ="Usuarios/imagen1.png";
-    const referencia= this.storage.ref(ruta)
-    const tarea = referencia.put(archivo)
-    tarea.then((objeto)=>{
-      referencia.getDownloadURL().subscribe((uri)=>{
-        console.log(uri)
+    if(evento.target.files.length>0){
+      let nombre=this.formularioClientes.value.Email
+      let archivo = evento.target.files[0]
+      let extension= archivo.name.toString().substring(archivo.name.toString().lastIndexOf('.'))
+      let ruta ="Usuarios/"+nombre;
+      const referencia= this.storage.ref(ruta)
+      const tarea = referencia.put(archivo)
+      tarea.then((objeto)=>{
+        referencia.getDownloadURL().subscribe((uri)=>{
+          console.log(uri)
+        })
       })
-    })
-    tarea.percentageChanges().subscribe((porcentaje)=>{
-      this.porcentajeSubida=parseInt(porcentaje.toString())
-    })
+      tarea.percentageChanges().subscribe((porcentaje)=>{
+        this.porcentajeSubida=parseInt(porcentaje.toString())
+      })
+  
+    }
 
-  }
+    }
+   
 }
--- src/app/listado-clientes/listado-clientes.component.ts
+++ src/app/listado-clientes/listado-clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import 'firebase/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-listado-clientes',
@@ -9,19 +10,24 @@ import 'firebase/firestore';
 })
 export class ListadoClientesComponent implements OnInit {
   clientes: any = new Array<any>();
-   constructor(private db: AngularFirestore) { }
+  arreglo: any = new Array<any>();
+   constructor(private db: AngularFirestore,private storage: AngularFireStorage) { }
 
   ngOnInit() {
     // this.db.collection('Usuarios').valueChanges().subscribe((resultado)=>{
     //   this.clientes=resultado
     //  })
     this.clientes.lenght=0
+    
     this.db.collection('Usuarios').get().subscribe((resultado)=>{
       resultado.docs.forEach((item)=>{
-
         let cliente = item.data();
         cliente.id=item.id;
         cliente.ref=item.ref;
+        const ref = this.storage.ref('Usuarios/'+cliente.id);
+        ref.getDownloadURL().subscribe((uri)=>{
+           this.arreglo[cliente.id]=uri
+         })
         this.clientes.push(cliente);
 
       })
