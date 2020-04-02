56edafa08c19ceb9cd2bade260ac6254234a018e
David <dlcabezas2@gmail.com>
2020-04-02 07:29:24 -0500

    Agregar Clientes Funciona en las dos bd
    Agregar Clientes Funciona en las dos bases de datos realtime y firestore

--- src/app/agregar-clientes/agregar-clientes.component.ts
+++ src/app/agregar-clientes/agregar-clientes.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AngularFireDatabase, AngularFireObject } from '@angular/fire/database';
 import { AngularFireStorage } from '@angular/fire/storage';
 import 'firebase/storage';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { MensajesService } from '../services/mensajes.service';
+import { Observable } from 'rxjs';
 
 
 
@@ -14,13 +16,19 @@ import { MensajesService } from '../services/mensajes.service';
 })
 export class AgregarClientesComponent implements OnInit {
   formularioClientes: FormGroup
+  itemRef: AngularFireObject<any>;
+  item: Observable<any>;
   constructor(private fb: FormBuilder,private storage: AngularFireStorage,private db: AngularFirestore,
-    private msj:MensajesService) { }
+    private msj:MensajesService,private rd:AngularFireDatabase) {
+      this.itemRef = rd.object('Usuarios');
+      this.item = this.itemRef.valueChanges();
+     }
   porcentajeSubida:number =0
   arreglo: any = new Array<any>();
   correo: string
   a = new Object();
   ngOnInit() {
+    
     this.formularioClientes=this.fb.group({
       Nombre: ['',Validators.required],
       Apellido: ['',Validators.required],
@@ -35,28 +43,46 @@ export class AgregarClientesComponent implements OnInit {
       
     })
   }
+ 
+  
   agregar(){
+    let Pin=Math.round(Math.random()*(1000-9999)+(9999));
     let correo: string=this.formularioClientes.value.Email
     let nombre: string=this.formularioClientes.value.Nombre
     let apellido: string=this.formularioClientes.value.Apellido
     let facultad: string=this.formularioClientes.value.Facultad
     let iden: string=this.formularioClientes.value.Iden
     let role: string=this.formularioClientes.value.Role
-    let carrera: string="Deshabilitado"
+    let carrera: string=""
+    let foto: string="no"
+    let pin : string="1234"
+    //Agregar a Clientes
     this.db.collection("Clientes").doc(iden).set({
       Email: correo,
-      Nid: iden
+      Nid: iden,
+      Pin: Pin
     }).then((resultado)=>{
       // console.log("Insertado en Clientes")
     });
-     this.db.collection("Usuarios").doc(correo).set({
-      Nombre: this.formularioClientes.value.Nombre,
+    //Agregar a Realtime Database
+    this.rd.object('/Usuarios/' + iden).update({
+      Facultad:facultad,
+      Perfil: role,
+      clave:Pin,
+      email:correo,
+      name: nombre,
+      uid:iden
+    });
+    //Agregar a Usuarios Firestore
+     this.db.collection("Usuarios").doc(iden).set({
+      Nombre: nombre,
       Apellido: apellido,
       Email: correo,
       Facultad: facultad,
       Iden: iden,
       Role: role,
-      Carrera: carrera
+      Carrera: carrera,
+      foto: foto,
   })
   .then((resultado)=>{
     this.msj.mensajecorrecto('Agregar','se Agrego Correctamente')
